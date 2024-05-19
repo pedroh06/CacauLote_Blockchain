22c6d3b70fe26d493804d5652c4b54efdea5ea96
pedroh06 <pedrohcsilva25@gmail.com>
2024-05-19 11:22:33 -0300

    third commit

--- demo/src/main/java/Conexao.java
+++ demo/src/main/java/Conexao.java
@@ -8,7 +8,7 @@ public class Conexao {
 
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://cacau-lote-db.mysql.database.azure.com:3306/teste_db?useSSL=true"; //teste
+        String url = "jdbc:mysql://cacau-lote-db.mysql.database.azure.com:3306/teste_db?useSSL=true";
         String user = "user";
         String password = "cacau#Lote";
 
