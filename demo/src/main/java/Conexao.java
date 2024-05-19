f00fb12a14d105cacf1502618851868096fb4827
pedroh06 <pedrohcsilva25@gmail.com>
2024-05-19 16:40:52 -0300

    finish

--- demo/src/main/java/Conexao.java
+++ demo/src/main/java/Conexao.java
@@ -2,12 +2,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Conexao {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws SQLException {
         // String url = "jdbc:mysql://localhost/cacaulotedb";
         // String user = "root";
         // String password = "2525";
@@ -17,23 +15,28 @@ public class Conexao {
         // ResultSet rsCacauDados = stmt.executeQuery("SELECT * FROM blocoGenesis"))
 
         Connection conexao = null;
-
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conexao = DriverManager.getConnection("jdbc:mysql://localhost/cacaulotedb", "root", "2525");
-            ResultSet rsBlocoGenesis = conexao.createStatement().executeQuery("SELECT * FROM blocoGenesis");
+            conexao = DriverManager.getConnection("jdbc:mysql://localhost/cacaulotedb", "pedro", "cacau#Lote");
+            ResultSet rsBlocoGenesis = conexao.createStatement().executeQuery("SELECT * FROM blocogenesis");
             while (rsBlocoGenesis.next()) {
                 String proprietario = rsBlocoGenesis.getString("nome_proprietario");
-                double latitude = rsBlocoGenesis.getDouble("latitude_plantacao");
+                double latitude = rsBlocoGenesis.getDouble("latitude");
+                double longitude = rsBlocoGenesis.getDouble("longitude");
 
                 System.out.println("Proprietario : " + proprietario);
                 System.out.println("Latitude : " + latitude);
+                System.out.println("Longitude : " + longitude);
             }
 
-        } catch (SQLException ex) {
-            System.out.println("Ocorreu um erro ao acessar o banco: " + ex.getMessage());
         } catch (ClassNotFoundException ex) {
             System.out.println("Driver do banco de dados não localizado");
+        } catch (SQLException ex) {
+            System.out.println("Ocorreu um erro ao acessar o banco: " + ex.getMessage());
+        } finally {
+            if (conexao != null) {
+                conexao.close();
+            }
         }
     }
 }
