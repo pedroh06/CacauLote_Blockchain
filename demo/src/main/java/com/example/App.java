2dcece0ae0a44be5da401ece48aa315dce6fce9f
pedroh06 <pedrohcsilva25@gmail.com>
2024-05-19 19:09:54 -0300

    teste4

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,4 +1,10 @@
 package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Blockchain blockchain = new Blockchain();
@@ -11,6 +17,31 @@ public class App {
 
         for (Block bloco : blockchain.getBlockchain()) {
             System.out.println(bloco.imprimirBloco());
+        }
+
+        Connection conexao = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexao = DriverManager.getConnection("jdbc:mysql://cacau-lote-db.mysql.database.azure.com:3306/cacaulotedb", "user", "cacau#Lote");
+            ResultSet rsBlocoGenesis = conexao.createStatement().executeQuery("SELECT * FROM bloco_Genesis");
+            while (rsBlocoGenesis.next()) {
+                String proprietario = rsBlocoGenesis.getString("nome_proprietario");
+                double latitude = rsBlocoGenesis.getDouble("latitude_plantacao");
+                double longitude = rsBlocoGenesis.getDouble("longitude_plantacao");
+
+                System.out.println("Proprietario : " + proprietario);
+                System.out.println("Latitude : " + latitude);
+                System.out.println("Longitude : " + longitude);
+            }
+
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Driver do banco de dados não localizado: " + ex.getMessage());
+        } catch (SQLException ex) {
+            System.out.println("Ocorreu um erro ao acessar o banco: " + ex.getMessage());
+        } finally {
+            if (conexao != null) {
+                conexao.close();
+            }
         }
     }
 }
