50be45ea4e24d0ae71780d77930973c26e31ceb5
pedroh06 <pedrohcsilva25@gmail.com>
2024-05-19 11:49:03 -0300

    forth commit

--- demo/src/main/java/Conexao.java
+++ demo/src/main/java/Conexao.java
@@ -8,16 +8,16 @@ public class Conexao {
 
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://cacau-lote-db.mysql.database.azure.com:3306/teste_db?useSSL=true";
-        String user = "user";
-        String password = "cacau#Lote";
+        String url = "jdbc:mysql://localhost:3306/cacaulotedb";
+        String user = "root";
+        String password = "2525";
 
         try (Connection conexao = DriverManager.getConnection(url, user, password);
                 Statement stmt = conexao.createStatement();
-                ResultSet rsCacauDados = stmt.executeQuery("SELECT * FROM blocogenesis")) {
+                ResultSet rsCacauDados = stmt.executeQuery("SELECT * FROM blocoGenesis")) {
 
             while (rsCacauDados.next()) {
-                String proprietario = rsCacauDados.getString("proprietario");
+                String proprietario = rsCacauDados.getString("nome_proprietario");
                 double latitude = rsCacauDados.getDouble("latitude_plantacao");
 
                 System.out.println("Proprietario : " + proprietario);
